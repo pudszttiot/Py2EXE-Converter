399ec352dd0f21e12064070db365e90ef803037d
Sourcery AI <>
2023-07-22 15:46:38 +0000

    'Refactored by Sourcery'

--- v1.2/Py2EXEConverter1.2.py
+++ v1.2/Py2EXEConverter1.2.py
@@ -50,19 +50,19 @@ class MainWindow(QMainWindow):
 
         # resizing the widget
         self.label_2.resize(500, 500)
-        
-  
+
+
         # setting up the border and adding image to background
         self.label_2.setStyleSheet("background-image : url(image.png)")
- 
+
 
         # show all the widgets
         self.show()
 
         # Lock the window size
         self.setFixedSize(self.size())
-  
-  
+
+
 
         label = QLabel("Select a Python file to convert:")
         label.setToolTip("")
@@ -105,9 +105,9 @@ class MainWindow(QMainWindow):
         }
 
         self.checkboxes = []
-        for option in self.options:
+        for option, value in self.options.items():
             checkbox = QCheckBox(option)
-            checkbox.setToolTip(self.options[option])
+            checkbox.setToolTip(value)
             layout.addWidget(checkbox)
             self.checkboxes.append(checkbox)
 
@@ -135,51 +135,52 @@ class MainWindow(QMainWindow):
             self.icon_button.setText("Icon file selected")
 
     def browse_output_folder(self):
-        folder_path = QFileDialog.getExistingDirectory(self, "Select output folder")
-        if folder_path:
+        if folder_path := QFileDialog.getExistingDirectory(
+            self, "Select output folder"
+        ):
             self.output_folder_path = folder_path
             self.output_button.setText("Output folder selected")
 
     def convert_to_exe(self):
-        if hasattr(self, "python_file_path"):
-            selected_options = [
-                option
-                for option, checkbox in zip(self.options, self.checkboxes)
-                if checkbox.isChecked()
-            ]
-            pyinstaller_command = (
-                ["pyinstaller"] + selected_options + [self.python_file_path]
+        if not hasattr(self, "python_file_path"):
+            return
+        selected_options = [
+            option
+            for option, checkbox in zip(self.options, self.checkboxes)
+            if checkbox.isChecked()
+        ]
+        pyinstaller_command = (
+            ["pyinstaller"] + selected_options + [self.python_file_path]
+        )
+
+        if hasattr(self, "icon_file_path"):
+            pyinstaller_command.insert(1, f"--icon={self.icon_file_path}")
+        if hasattr(self, "output_folder_path"):
+            build_folder_path = os.path.join(self.output_folder_path, "build")
+            pyinstaller_command.extend(
+                [
+                    "--distpath",
+                    self.output_folder_path,
+                    "--workpath",
+                    build_folder_path,
+                ]
             )
+            spec_file_path = os.path.join(self.output_folder_path, "output.spec")
+            pyinstaller_command.extend(
+                ["--specpath", self.output_folder_path, "--name", spec_file_path]
+            )
+        if app_name := self.name_input.text():
+            pyinstaller_command.append(f"--name={app_name}")
+        result = subprocess.call(pyinstaller_command)
 
-            if hasattr(self, "icon_file_path"):
-                pyinstaller_command.insert(1, f"--icon={self.icon_file_path}")
-            if hasattr(self, "output_folder_path"):
-                build_folder_path = os.path.join(self.output_folder_path, "build")
-                pyinstaller_command.extend(
-                    [
-                        "--distpath",
-                        self.output_folder_path,
-                        "--workpath",
-                        build_folder_path,
-                    ]
-                )
-                spec_file_path = os.path.join(self.output_folder_path, "output.spec")
-                pyinstaller_command.extend(
-                    ["--specpath", self.output_folder_path, "--name", spec_file_path]
-                )
-            app_name = self.name_input.text()
-            if app_name:
-                pyinstaller_command.append(f"--name={app_name}")
-            result = subprocess.call(pyinstaller_command)
-
-            if result == 0:
-                QMessageBox.information(
-                    self, "Conversion Complete", "Conversion to EXE was successful!"
-                )
-            else:
-                QMessageBox.critical(
-                    self, "Conversion Failed", "Conversion to EXE failed!"
-                )
+        if result == 0:
+            QMessageBox.information(
+                self, "Conversion Complete", "Conversion to EXE was successful!"
+            )
+        else:
+            QMessageBox.critical(
+                self, "Conversion Failed", "Conversion to EXE failed!"
+            )
 
 
 if __name__ == "__main__":
